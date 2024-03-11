c8a08527753ab5abc1265cacbd113abc99698085
Karol T <krol22ee@gmail.com>
2024-03-11 21:29:31 +0100

    Readding norg

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "javascript", "typescript", "go", "lua", "rust" },
+  ensure_installed = { "javascript", "typescript", "go", "c", "lua", "rust", "norg" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
